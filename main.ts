33c02317660458827afc8dce7b24a04f2c5e641c
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-24 10:52:40 -0400

    Made traffic lights

--- main.ts
+++ main.ts
@@ -1,21 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Joos
+ * Created on: Oct 2024
  * This program USes neostrips to make a traffic light
 */
 
 let neopixelStrip: neopixel.Strip = null
-
-basic.clearScreen()
-neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function() {
 
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Square)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))})
+//turns neopixel green
+input.buttonIsPressed(Button.A)
+neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(2,neopixel.colors(NeoPixelColors.Green))
+neopixelStrip.show()
+basic.pause(2000)
+
+//turns neopixel yellow
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    basic.pause(2000)
+
+//turns neopixel red
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.pause(2000)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+})
